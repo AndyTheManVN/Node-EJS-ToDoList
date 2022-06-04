4fccd8b178e30f7c72cf033088f0b3f79ca732a9
andydangVN <dangminhnghia@gmail.com>
2022-06-04 16:28:27 +0700

    Show day with diff color weekday and weekend

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-<strong>EJS : Embedded JavaScript</strong>
+<h1><strong>EJS : Embedded JavaScript</strong><h1>
 
 1. EJS:
   - Using EJS with Expres: https://github.com/mde/ejs/wiki/Using-EJS-with-Express
   - Cài đặt: npm i ejs
-3. Views
-4. 
+2. Views hoặc Template:
+  - Tạo folder views (chữ v phải viết thường)
+  - Tạo toàn bộ các file view trong folder này (tương tự template trong Django).
+  - Render dữ liệu từ app.js ra view (ví dụ là view list - list.ejs)
+    ```res.render("list", {day: day});``` 
+3. Cú pháp của EJS trong html:
+  - **<%** not html tag **%>**: thoát câu lệnh html
+  - **<%=** variable **%>**: xuất giá trị biến ra html
 
--- app.js
+++ app.js
@@ -16,13 +16,32 @@ app.get('/', function(req, res){
   var currentDay = today.getDay();
   var day = "";
 
-  if (currentDay == 0 || currentDay == 6) {
-    day = "Weekend";
-     
-  } else {
-    day = "Weekday";
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to " + currentDay);
   }
 
-  res.render("list", {KindOfDay: day});   
+  res.render("list", {day: day});   
 });
 
